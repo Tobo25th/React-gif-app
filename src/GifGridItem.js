0c254775f0f3d3bddac365893293b96255a71f8a
Tobo25th <95103928+Tobo25th@users.noreply.github.com>
2022-04-26 08:45:54 -0300

    Update GifGridItem.js

--- src/GifGridItem.js
+++ src/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GifGridItem = ({id,title,url}) => {
     return (
-    <div className="wachin animate__animated animate__bounce">
+    <div className="gif-item animate__animated animate__bounce">
         <img src={url} alt={title}/>
     </div>
   )
